6f698a645812778ed856ee3abcda7f834c30c52b
Jeff Oryen <jeff@solvhealth.com>
2022-08-23 15:45:33 -0700

    cleanup init.lua file

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,4 +1,6 @@
 require('packer').startup(function(use)
+    require("lsp").setup()
+
     use { 'alexghergh/nvim-tmux-navigation', config = function()
         local nvim_tmux_nav = require('nvim-tmux-navigation')
         nvim_tmux_nav.setup {
@@ -15,14 +17,17 @@ require('packer').startup(function(use)
     end }
 
     use 'kyazdani42/nvim-web-devicons'
+
     use { 'nvim-lualine/lualine.nvim', config = function()
+        vim.api.nvim_set_option("termguicolors", true)
         require("lualine").setup({
             options = { theme = "gruvbox" },
             sections = { lualine_c = { "filename", require('lsp').active } }
         })
     end }
+
     use 'neovim/nvim-lspconfig'
-    use 'echasnovski/mini.nvim'
+
     use { 'ibhagwan/fzf-lua', branch = 'main', config = function()
         local actions = require("fzf-lua.actions")
         require("fzf-lua").setup({
@@ -37,13 +42,17 @@ require('packer').startup(function(use)
             },
         })
     end }
+
     use 'kdheepak/lazygit.nvim'
+
     use { 'williamboman/mason.nvim', config = function()
         require("mason").setup({})
     end }
+
     use { 'williamboman/mason-lspconfig.nvim', config = function()
         require("mason-lspconfig").setup({})
     end }
+
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function()
         require("nvim-treesitter.configs").setup({
             ensure_installed = { "python", "vim", "lua", "javascript", "go" },
@@ -53,75 +62,67 @@ require('packer').startup(function(use)
             },
         })
     end }
+
     use { 'norcalli/nvim-colorizer.lua', config = function()
+        vim.api.nvim_set_option("termguicolors", true)
         require("colorizer").setup()
     end }
-end)
 
 
--- must be set before plugins that deal with colors
-vim.api.nvim_set_option("termguicolors", true)
+    use { 'echasnovski/mini.nvim', branch = 'stable', config = function()
+        vim.api.nvim_set_option("termguicolors", true)
+        require("mini.bufremove").setup({})
+        require("mini.comment").setup({})
+        require("mini.completion").setup({})
+        require("mini.cursorword").setup({})
+        require("mini.indentscope").setup({})
+        require("mini.jump").setup({})
+        require("mini.surround").setup({})
+        require("mini.tabline").setup({})
+    end }
 
-require("lsp").setup()
-require("mini.bufremove").setup({})
-require("mini.comment").setup({})
-require("mini.completion").setup({})
-require("mini.cursorword").setup({})
-require("mini.indentscope").setup({})
-require("mini.jump").setup({})
-require("mini.surround").setup({})
-require("mini.tabline").setup({})
 
-
--- ===========================================
--- ====== nvim-treesitter configuration ======
--- ===========================================
--- -------------------------------------------
-
--- ===========================================
--- ======== mini.base16 configuration ========
--- ===========================================
-local palettes = {
-    dark = {
-        base00 = "#262626", -- # ----
-        base01 = "#3a3a3a", -- # ---
-        base02 = "#4e4e4e", -- # --
-        base03 = "#8a8a8a", -- # -
-        base04 = "#949494", -- # +
-        base05 = "#dab997", -- # ++
-        base06 = "#d5c4a1", -- # +++
-        base07 = "#ebdbb2", -- # ++++
-        base08 = "#d75f5f", -- # red
-        base09 = "#ff8700", -- # orange
-        base0A = "#ffaf00", -- # yellow
-        base0B = "#afaf00", -- # green
-        base0C = "#85ad85", -- # aqua/cyan
-        base0D = "#83adad", -- # blue
-        base0E = "#d485ad", -- # purple
-        base0F = "#d65d0e", -- # brown
-    },
-    light = {
-        base00 = "#fbf1c7", -- # ----
-        base01 = "#ebdbb2", -- # ---
-        base02 = "#d5c4a1", -- # --
-        base03 = "#bdae93", -- # -
-        base04 = "#665c54", -- # +
-        base05 = "#504945", -- # ++
-        base06 = "#3c3836", -- # +++
-        base07 = "#282828", -- # ++++
-        base08 = "#9d0006", -- # red
-        base09 = "#af3a03", -- # orange
-        base0A = "#b57614", -- # yellow
-        base0B = "#79740e", -- # green
-        base0C = "#427b58", -- # aqua/cyan
-        base0D = "#076678", -- # blue
-        base0E = "#8f3f71", -- # purple
-        base0F = "#d65d0e", -- # brown
+    local palettes = {
+        dark = {
+            base00 = "#262626", -- # ----
+            base01 = "#3a3a3a", -- # ---
+            base02 = "#4e4e4e", -- # --
+            base03 = "#8a8a8a", -- # -
+            base04 = "#949494", -- # +
+            base05 = "#dab997", -- # ++
+            base06 = "#d5c4a1", -- # +++
+            base07 = "#ebdbb2", -- # ++++
+            base08 = "#d75f5f", -- # red
+            base09 = "#ff8700", -- # orange
+            base0A = "#ffaf00", -- # yellow
+            base0B = "#afaf00", -- # green
+            base0C = "#85ad85", -- # aqua/cyan
+            base0D = "#83adad", -- # blue
+            base0E = "#d485ad", -- # purple
+            base0F = "#d65d0e", -- # brown
+        },
+        light = {
+            base00 = "#fbf1c7", -- # ----
+            base01 = "#ebdbb2", -- # ---
+            base02 = "#d5c4a1", -- # --
+            base03 = "#bdae93", -- # -
+            base04 = "#665c54", -- # +
+            base05 = "#504945", -- # ++
+            base06 = "#3c3836", -- # +++
+            base07 = "#282828", -- # ++++
+            base08 = "#9d0006", -- # red
+            base09 = "#af3a03", -- # orange
+            base0A = "#b57614", -- # yellow
+            base0B = "#79740e", -- # green
+            base0C = "#427b58", -- # aqua/cyan
+            base0D = "#076678", -- # blue
+            base0E = "#8f3f71", -- # purple
+            base0F = "#d65d0e", -- # brown
+        }
     }
-}
 
-require("mini.base16").setup({
-    palette = palettes["dark"],
-    use_cterm = true,
-})
--- -------------------------------------------
+    require("mini.base16").setup({
+        palette = palettes["dark"],
+        use_cterm = true,
+    })
+end)
