2aba702290d5b35511d20a119e7c72ed512077df
Jeff Oryen <jeff@solvhealth.com>
2022-08-25 09:17:27 -0700

    update packer to auto install on new installs

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,4 +1,10 @@
-vim.cmd [[packadd packer.nvim]]
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+        install_path })
+    vim.cmd [[packadd packer.nvim]]
+end
 
 require('packer').startup(function(use)
     vim.api.nvim_set_option("termguicolors", true)
@@ -127,4 +133,17 @@ require('packer').startup(function(use)
         use_cterm = true,
     })
 
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+
 end)
+
+vim.cmd [[packadd packer.nvim]]
+
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]]
