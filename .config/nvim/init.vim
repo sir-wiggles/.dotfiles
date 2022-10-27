bfb0d1d815e97b9d17e1717eb9afb4bb3b9f7d6a
Jeff Oryen <jeffor@zg14958.lan>
2022-10-26 21:56:48 -0700

    revert to using vim-plug

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,5 +1,39 @@
+call plug#begin('~/.config/nvim/plugged')
+    Plug 'mfussenegger/nvim-dap'
+    Plug 'rcarriga/nvim-dap-ui'
+    " seamless navigation when using vim in tmux
+    Plug 'alexghergh/nvim-tmux-navigation'                      " lua
+    "
+    " pretty icons in vim, a must have
+    Plug 'kyazdani42/nvim-web-devicons'                         " lua
+    Plug 'nvim-lualine/lualine.nvim'                            " lua
+
+    Plug 'neovim/nvim-lspconfig'                                " lua
+    Plug 'echasnovski/mini.nvim'                                " lua
+    Plug 'ibhagwan/fzf-lua', {'branch': 'main'}                 " lua
+    Plug 'kdheepak/lazygit.nvim'                                " lua
+
+    " manager of various lsp servers
+    Plug 'williamboman/mason.nvim'                              " lua
+    Plug 'williamboman/mason-lspconfig.nvim'                    " lua
+
+    " better syntax highlighting
+    Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'} " lua
+
+    " ========= experimental plugins ==========
+    Plug 'norcalli/nvim-colorizer.lua'                          " lua
+    Plug 'mhinz/vim-startify'                                   " vim
+    "Plug 'lifepillar/vim-mucomplete'                            " vim
+
+    " ========= experimental plugins ==========
+call plug#end()
+
+" Setup all the Lua plugins
 lua require('init')
 
+" ===========================================
+" ================ options ==================
+" ===========================================
 set clipboard+=unnamedplus
 set cmdheight=1
 set colorcolumn=90
@@ -21,30 +55,80 @@ set splitright
 set tabstop=4
 set wildmenu
 set wildmode=longest:full,full
+" -------------------------------------------
 
-let mapleader = ','
-
-inoremap jk <esc>
-inoremap JK <esc>
-
+" ===========================================
+" =============== navigation ================
+" ===========================================
+" ------------------ tmux -------------------
+nnoremap <silent> <C-h>     :lua require('nvim-tmux-navigation').NvimTmuxNavigateLeft()<cr>
+nnoremap <silent> <C-j>     :lua require('nvim-tmux-navigation').NvimTmuxNavigateDown()<cr>
+nnoremap <silent> <C-k>     :lua require('nvim-tmux-navigation').NvimTmuxNavigateUp()<cr>
+nnoremap <silent> <C-l>     :lua require('nvim-tmux-navigation').NvimTmuxNavigateRight()<cr>
+nnoremap <silent> <C-\>     :lua require('nvim-tmux-navigation').NvimTmuxNavigateLastActive()<cr>
+nnoremap <silent> <C-Space> :lua require('nvim-tmux-navigation').NvimTmuxNavigateNext()<cr>
+" ------------------ tab --------------------
 nnoremap <leader><c-h> :tabprevious<cr>
 nnoremap <leader><c-l> :tabnext<cr>
-
+" ----------------- buffer ------------------
 nnoremap <s-l> :bn<cr>
 nnoremap <s-h> :bp<cr>
 nnoremap <c-c> :bw<cr>
+" -------------------------------------------
 
+" ===========================================
+" ============== copy + paste ===============
+" ===========================================
+vnoremap <leader>y  "+y
+nnoremap <leader>Y  "+yg_
+nnoremap <leader>y  "+y
+nnoremap <leader>yy "+yy
+nnoremap <leader>ff :let @+ = expand("%")<cr>
+vnoremap <leader>p  "+p
+vnoremap <leader>P  "+P
+nnoremap <leader>p  "+p
+nnoremap <leader>P  "+P
+" -------------------------------------------
+
+" ===========================================
+" ============= =============== =============
+" ===========================================
+" --------------- variables -----------------
+let mapleader = ','
+let g:startify_fortune_use_unicode=1
+" ---------------- i-remaps -----------------
+inoremap jk <esc>
+inoremap JK <esc>
+" ---------------- n-remaps -----------------
+nnoremap <leader>h :setlocal spell! spelllang=en_us<cr>
+nnoremap <c-w> :nohlsearch<cr>
+nnoremap <c-p> <cmd>lua require('fzf-lua').files()<cr>
+" resize buffers relative to others in the vim session
 nnoremap <s-left>  :vertical resize -5<cr>
 nnoremap <s-right> :vertical resize +5<cr>
 nnoremap <s-down>  :resize          +5<cr>
 nnoremap <s-up>    :resize          -5<cr>
-
-nnoremap <leader>h :setlocal spell! spelllang=en_us<cr>
-nnoremap <c-w> :nohlsearch<cr>
-nnoremap <c-p> <cmd>lua require('fzf-lua').files()<cr>
-
+" ---------------- commands -----------------
 command! Grep lua require('fzf-lua').grep()
 command! Git  :LazyGit
 
-autocmd! TextYankPost * silent! lua vim.highlight.on_yank({timeout=400})
-autocmd! BufWritePre * :%s/\s\+$//e
+" ---------------- autocmds -----------------
+" highlight the yanked text for a short period of time
+augroup YankHighlight
+    autocmd! TextYankPost * silent! lua vim.highlight.on_yank({timeout=400})
+augroup end
+" strip trailing white space at the end of lines
+autocmd BufWritePre * :%s/\s\+$//e
+" -------------------------------------------
+
+
+
+nnoremap <silent> <F5> <Cmd>lua require'dap'.continue()<CR>
+nnoremap <silent> <F10> <Cmd>lua require'dap'.step_over()<CR>
+nnoremap <silent> <F11> <Cmd>lua require'dap'.step_into()<CR>
+nnoremap <silent> <F12> <Cmd>lua require'dap'.step_out()<CR>
+nnoremap <silent> <Leader>b <Cmd>lua require'dap'.toggle_breakpoint()<CR>
+nnoremap <silent> <Leader>B <Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
+nnoremap <silent> <Leader>lp <Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
+nnoremap <silent> <Leader>dr <Cmd>lua require'dap'.repl.open()<CR>
+nnoremap <silent> <Leader>dl <Cmd>lua require'dap'.run_last()<CR>
--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,157 +1,227 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+-- must be set before plugins that deal with colors
+vim.api.nvim_set_option("termguicolors", true)
+
+require("lsp").setup()
+require("colorizer").setup()
+require("mason").setup({})
+require("mason-lspconfig").setup({})
+require("mini.bufremove").setup({})
+require("mini.comment").setup({})
+require("mini.completion").setup({})
+require("mini.cursorword").setup({})
+require("mini.indentscope").setup({})
+require("mini.jump").setup({})
+require("mini.surround").setup({})
+require("mini.tabline").setup({})
+
+-- ===========================================
+-- ====== nvim-treesitter configuration ======
+-- ===========================================
+require("nvim-treesitter.configs").setup({
+    ensure_installed = { "python", "vim", "lua", "javascript", "go" },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+})
+-- -------------------------------------------
+
+-- ===========================================
+-- ========== fzf-lua configuration ==========
+-- ===========================================
+-- override the default action of sending selections to quickfix and just
+-- edit the files.
+local actions = require("fzf-lua.actions")
+require("fzf-lua").setup({
+    actions = {
+        files = {
+            ["default"] = actions.file_edit,
+            ["ctrl-s"]  = actions.file_split,
+            ["ctrl-v"]  = actions.file_vsplit,
+            ["ctrl-t"]  = actions.file_tabedit,
+            ["alt-q"]   = actions.file_sel_to_qf,
+        },
+    },
+})
+-- -------------------------------------------
+
+-- ===========================================
+-- ======== mini.base16 configuration ========
+-- ===========================================
+local palettes = {
+    dark = {
+        base00 = "#262626", -- # ----
+        base01 = "#3a3a3a", -- # ---
+        base02 = "#4e4e4e", -- # --
+        base03 = "#8a8a8a", -- # -
+        base04 = "#949494", -- # +
+        base05 = "#dab997", -- # ++
+        base06 = "#d5c4a1", -- # +++
+        base07 = "#ebdbb2", -- # ++++
+        base08 = "#d75f5f", -- # red
+        base09 = "#ff8700", -- # orange
+        base0A = "#ffaf00", -- # yellow
+        base0B = "#afaf00", -- # green
+        base0C = "#85ad85", -- # aqua/cyan
+        base0D = "#83adad", -- # blue
+        base0E = "#d485ad", -- # purple
+        base0F = "#d65d0e", -- # brown
+    },
+    light = {
+        base00 = "#fbf1c7", -- # ----
+        base01 = "#ebdbb2", -- # ---
+        base02 = "#d5c4a1", -- # --
+        base03 = "#bdae93", -- # -
+        base04 = "#665c54", -- # +
+        base05 = "#504945", -- # ++
+        base06 = "#3c3836", -- # +++
+        base07 = "#282828", -- # ++++
+        base08 = "#9d0006", -- # red
+        base09 = "#af3a03", -- # orange
+        base0A = "#b57614", -- # yellow
+        base0B = "#79740e", -- # green
+        base0C = "#427b58", -- # aqua/cyan
+        base0D = "#076678", -- # blue
+        base0E = "#8f3f71", -- # purple
+        base0F = "#d65d0e", -- # brown
+    }
+}
+
+require("mini.base16").setup({
+    palette = palettes["dark"],
+    use_cterm = true,
+})
+-- -------------------------------------------
+
+-- ===========================================
+-- ========== lualine configuration ==========
+-- ===========================================
+-- this plugin must be called after mini.base16
+require("lualine").setup({
+    options = { theme = "gruvbox" },
+    sections = { lualine_c = { "filename", require('lsp').active } }
+})
+
+
+local dap = require('dap')
+
+local venv = os.getenv("VIRTUAL_ENV")
+local env = '/Users/jeffor/.pyenv/versions/3.10.7/envs/test/bin/python'
+dap.adapters.python = {
+  type = 'executable';
+  command = env;
+  args = { '-m', 'debugpy.adapter' };
+}
+
+local dap = require('dap')
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch';
+    name = "Launch file";
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}"; -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        return env
+        -- return '/usr/bin/python'
+      end
+    end;
+  },
+}
+
+require("dapui").setup({
+  icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  -- Expand lines larger than the window
+  -- Requires >= 0.7
+  expand_lines = vim.fn.has("nvim-0.7") == 1,
+  -- Layouts define sections of the screen to place windows.
+  -- The position can be "left", "right", "top" or "bottom".
+  -- The size specifies the height/width depending on position. It can be an Int
+  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+  -- Elements are the elements shown in the layout (in order).
+  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  layouts = {
+    {
+      elements = {
+      -- Elements can be strings or table with id and size keys.
+        { id = "scopes", size = 0.25 },
+        "breakpoints",
+        "stacks",
+        "watches",
+      },
+      size = 40, -- 40 columns
+      position = "left",
+    },
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 0.25, -- 25% of total lines
+      position = "bottom",
+    },
+  },
+  controls = {
+    -- Requires Neovim nightly (or 0.8 when released)
+    enabled = true,
+    -- Display controls in this element
+    element = "repl",
+    icons = {
+      pause = "",
+      play = "",
+      step_into = "",
+      step_over = "",
+      step_out = "",
+      step_back = "",
+      run_last = "↻",
+      terminate = "□",
+    },
+  },
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+    max_value_lines = 100, -- Can be integer or nil.
+  }
+})
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
 end
 
-local packer_bootstrap = ensure_packer()
-
-require('packer').startup(function(use)
-    vim.api.nvim_set_option("termguicolors", true)
-
-    use 'wbthomason/packer.nvim'
-
-    use { 'alexghergh/nvim-tmux-navigation', config = function()
-        local nvim_tmux_nav = require('nvim-tmux-navigation')
-        nvim_tmux_nav.setup {
-            disable_when_zoomed = false -- defaults to false
-        }
-
-        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-        vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-        vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
-    end }
-
-    use 'kyazdani42/nvim-web-devicons'
-
-    use { 'nvim-lualine/lualine.nvim', config = function()
-        require("lualine").setup({
-            options = { theme = "gruvbox" },
-            sections = { lualine_c = { "filename", require('lsp').active } }
-        })
-    end }
-
-    use 'neovim/nvim-lspconfig'
-
-    use { 'ibhagwan/fzf-lua', branch = 'main', config = function()
-        local actions = require("fzf-lua.actions")
-        require("fzf-lua").setup({
-            actions = {
-                files = {
-                    ["default"] = actions.file_edit,
-                    ["ctrl-s"]  = actions.file_split,
-                    ["ctrl-v"]  = actions.file_vsplit,
-                    ["ctrl-t"]  = actions.file_tabedit,
-                    ["alt-q"]   = actions.file_sel_to_qf,
-                },
-            },
-        })
-    end }
-
-    use 'kdheepak/lazygit.nvim'
-
-    use { 'williamboman/mason.nvim', config = function()
-        require("mason").setup({})
-    end }
-
-    use { 'williamboman/mason-lspconfig.nvim', config = function()
-        require("mason-lspconfig").setup({})
-    end }
-
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "python", "vim", "lua", "javascript", "go", "c" },
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end }
-
-    use { 'norcalli/nvim-colorizer.lua', config = function()
-        require("colorizer").setup()
-    end }
-
-
-    use { 'echasnovski/mini.nvim', branch = 'stable', config = function()
-        require("mini.bufremove").setup({})
-        require("mini.comment").setup({})
-        require("mini.completion").setup({})
-        require("mini.cursorword").setup({})
-        require("mini.indentscope").setup({})
-        require("mini.jump").setup({})
-        require("mini.surround").setup({})
-        require("mini.tabline").setup({})
-    end }
-
-
-    local palettes = {
-        dark = {
-            base00 = "#262626", -- # ----
-            base01 = "#3a3a3a", -- # ---
-            base02 = "#4e4e4e", -- # --
-            base03 = "#8a8a8a", -- # -
-            base04 = "#949494", -- # +
-            base05 = "#dab997", -- # ++
-            base06 = "#d5c4a1", -- # +++
-            base07 = "#ebdbb2", -- # ++++
-            base08 = "#d75f5f", -- # red
-            base09 = "#ff8700", -- # orange
-            base0A = "#ffaf00", -- # yellow
-            base0B = "#afaf00", -- # green
-            base0C = "#85ad85", -- # aqua/cyan
-            base0D = "#83adad", -- # blue
-            base0E = "#d485ad", -- # purple
-            base0F = "#d65d0e", -- # brown
-        },
-        light = {
-            base00 = "#fbf1c7", -- # ----
-            base01 = "#ebdbb2", -- # ---
-            base02 = "#d5c4a1", -- # --
-            base03 = "#bdae93", -- # -
-            base04 = "#665c54", -- # +
-            base05 = "#504945", -- # ++
-            base06 = "#3c3836", -- # +++
-            base07 = "#282828", -- # ++++
-            base08 = "#9d0006", -- # red
-            base09 = "#af3a03", -- # orange
-            base0A = "#b57614", -- # yellow
-            base0B = "#79740e", -- # green
-            base0C = "#427b58", -- # aqua/cyan
-            base0D = "#076678", -- # blue
-            base0E = "#8f3f71", -- # purple
-            base0F = "#d65d0e", -- # brown
-        }
-    }
-
-    require("mini.base16").setup({
-        palette = palettes["dark"],
-        use_cterm = true,
-    })
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-
-  require("lsp").setup()
-
-end)
-
-vim.cmd [[packadd packer.nvim]]
-
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]]
